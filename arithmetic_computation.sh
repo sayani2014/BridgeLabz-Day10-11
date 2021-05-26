bc9a2c1c93a172c7a7fb42838ccf9cef03bf772d
sayani2014 <sayanikoley8@gmail.com>
2021-05-26 12:06:07 +0530

    Store results in the dictionary UC

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -1,16 +1,23 @@
 #echo "Sorting Arithmetic Computation Problem"
 
+declare -A dict
 read -p "Enter 3 numbers as an input: " a b c
 #echo $a $b $c
 
 result1=$(($a + $b * $c))
 echo "Result1: $result1"
+dict["1"]=$result1
 
 result2=$(($a * $b + $c))
 echo "Result2: $result2"
+dict["2"]=$result2
 
 result3=$(($c + $a / $b))
 echo "Result3: $result3"
+dict["3"]=$result3
 
 result4=$(($a % $b + $c))
 echo "Result4: $result4"
+dict["4"]=$result4
+
+echo "Dictionary storage: ${dict[*]}"
